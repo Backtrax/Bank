e463fc687d59b5d299b3c0ccb9ec15eccfd0f02d
Backtrax <www.gaurav.info@gmail.com>
2015-07-09 12:42:08 +0530

    java]

--- Bank.java
+++ Bank.java
@@ -1,282 +1,134 @@
- import java.util.Scanner;  
- import java.util.Random;  
- import java.lang.Exception;  
-   
- class MyException extends Exception  
- {  
-      MyException(String message)  
-      {  
-           super(message);  
-      }  
- }  
-   
- interface Account  
- {  
-      void CreateAcc();  
-      void Deposit();  
-      void Withdraw();  
- }  
-   
- class BankUtilities  
- {  
-      int acctype,year=1,ssn=0,accnum;  
-      float intr;  
-      String name="";  
-      String loc="";  
-      Scanner in = new Scanner(System.in);  
-      Random rnd = new Random();  
-      double temp=0.0,bal=0.0;  
-        
- }  
-   
- class Bank extends BankUtilities implements Account  
- {  
-        
-        
-       void getInfo()  
-       {  
-           try  
-           {       
-                  
-                System.out.print("Enter Name: ");  
-                name=in.nextLine();  
-                  
-                  
-                System.out.print("Enter SSN: ");  
-                ssn=in.nextInt();  
-                  
-                in.nextLine();  
-                System.out.print("Enter Location: ");  
-                loc=in.nextLine();  
-                  
-                     System.out.println("Mention account type:\n1.Savings (7% intr )\t2.Current (5% intr) ");  
-                     acctype=in.nextInt();  
-                     switch(acctype)  
-                     {  
-                          case 1:  
-                               System.out.println("Enter the initial amount of deposit:");  
-                                temp=in.nextFloat();  
-                               if(temp<0)  
-                               {  
-                                    System.out.println("Invalid Amount\nTry again:\n");  
-                                    System.out.println("Enter the initial amount of deposit:");  
-                                    temp=in.nextFloat();  
-                               }  
-                               Deposit(temp);  
-                               System.out.println("Enter noof years:");  
-                               year=in.nextInt();  
-                               if(year<=0)  
-                               {  
-                                    System.out.println("Invalid year\n Try again.");  
-                                    System.out.println("Enter noof years:");  
-                                    year=in.nextInt();  
-                               }  
-                                 
-                          break;  
-                            
-                          case 2:  
-                               System.out.println("Enter the initial amount of deposit:");  
-                                temp=in.nextFloat();  
-                                if(temp<0)  
-                               {  
-                                    System.out.println("Invalid Amount\nTry again:\n");  
-                                    System.out.println("Enter the initial amount of deposit:");  
-                                    temp=in.nextFloat();  
-                               }  
-                               Deposit(temp);  
-                          break;  
-                            
-                          default: System.out.println("Invalid Option");  
-                     } //Switch  
-                  
-                  
-           }  
-           catch(Exception e)  
-           {  
-                System.out.println("Inbuilt Exception --> "+e);  
-                System.exit(0);  
-           }  
-        
-      }//getInfo  
-        
-      public void CreateAcc()   
-      {  
-           try  
-           {  
-           getInfo();  
-           System.out.println("\nAccount Successfully Created!");  
-           accnum=rnd.nextInt(1000)+1;  
-           System.out.println("Hello "+name+" your account no is " +accnum+".\n");  
-           }  
-           catch(Exception e)  
-           {  
-                System.out.println("Fatal Error");  
-           }  
-      }  
-        
-      void Deposit(double temp) // Initial Deposit  
-      {  
-           try  
-           {  
-                if(temp>500)  
-                {  
-                     bal+=temp;  
-                     System.out.println("SUCCESSFULLY CREDITED");  
-                }  
-                else  
-                {  
-                     throw new MyException("Minimum Deposit Violation");  
-                }  
-           }  
-           catch(MyException e)  
-           {  
-                System.out.println(e.getMessage());  
-                System.out.println("TRANSACTION FAILURE");  
-                System.exit(0);  
-           }  
-        
-      }  
-        
-      public void Deposit() // regular deposit  
-      {  
-        
-           System.out.println("Enter the amount to be deposited :");  
-           temp=in.nextFloat();  
-           try  
-           {  
-                if(temp>0)  
-                {  
-                     bal+=temp;  
-                     System.out.println("SUCCESSFULLY CREDITED");  
-                }  
-                else  
-                {  
-                     throw new MyException("INVALID AMOUNT EXCEPTION");  
-                }  
-           }  
-           catch(MyException e)  
-           {  
-                System.out.println(e.getMessage());  
-                System.out.println("TRANSACTION FAILURE");  
-           }  
-        
-      }  
-        
-      public void Withdraw()// regular deposit  
-      {  
-        
-           System.out.println("Enter the amount to be withdrawn :");  
-           temp=in.nextFloat();  
-           if(temp<=0)  
-           {  
-                System.out.println("Invalid Amount Error.");  
-                System.exit(0);  
-           }  
-             
-           try  
-           {  
-        
-                if(temp<bal)  
-                {  
-                     bal-=temp;  
-                     System.out.println("SUCCESSFULLY DEBITED");  
-                }  
-                else  
-                {  
-                     throw new MyException("INSUFFIECEINT FUND EXCEPTION");  
-                }  
-           }  
-           catch(MyException e)  
-           {  
-                System.out.println(e.getMessage());  
-                System.out.println("TRANSACTION FAILURE");  
-           }  
-        
-      }  
-      void Interest()  
-      {  
-           try  
-           {  
-                if("".equals(name)&&(ssn==0))  
-                {  
-                     throw new MyException("ACCONT NOT FOUND EXCEPTION");  
-                }  
-                else  
-                {  
-                     if(acctype==1)  
-                     {  
-                          bal+=year*bal*0.07;  
-                          System.out.println("7% interest added");  
-                     }  
-                     else if(acctype==2)  
-                     {  
-                          bal+=year*bal*0.03;  
-                          System.out.println("3% interest added");  
-                     }  
-                }  
-           }  
-           catch(MyException e)  
-           {  
-                System.out.println(e.getMessage());  
-                System.out.println("TRANSACTION FAILURE");  
-           }  
-      }  
-        
-      void CheckBal()  
-      {  
-           System.out.println("Balance:"+bal);  
-      }  
-        
- }  
-   
-   
- class BankDemoExc   
- {  
-      public static void main(String arg[]) throws MyException  
-      {  
-           Scanner in = new Scanner(System.in);  
-           Bank b = new Bank();  
-             
-           System.out.println("\n\n\t----Bank Simulator----");  
-           try  
-           {  
-                while(true)  
-                {  
-           System.out.println("1.Create Account\t2.Check Balance \n3.Deposit\t\t4.Withdraw\n5.Interest\t\t6.Exit.");  
-                int ch = in.nextInt();  
-                     switch(ch)  
-                     {  
-                          case 1:  
-                          b.CreateAcc();  
-                          break;  
-                            
-                          case 2:  
-                          b.CheckBal();  
-                          break;  
-                            
-                          case 3:  
-                          b.Deposit();  
-                          break;  
-                            
-                          case 4:  
-                          b.Withdraw();  
-                          break;  
-                            
-                          case 5:  
-                          b.Interest();  
-                          break;  
-                            
-                          case 6:  
-                          System.exit(0);  
-                          break;  
-                            
-                          default: System.out.println("Invalid Option");  
-                     }  
-                }  
-           }  
-           catch(Exception e)  
-           {  
-                System.out.println("SELF THROWN EXCEPTION IS--->"+e);  
-           }  
-      }  
- }  
+import java.io.*;
+class Bank
+{
+int[][] det=new int[100][4];
+float[] bal=new float[100];
+String[] name=new String[100];
+int count=0;
+void intialize(int t,float b,String d)
+{
+det[count][0]=count+1;
+det[count][1]=t;
+bal[count]=b;
+name[count]=d;
+System.out.println("Account No is: "+count+1);
+count++;
+}
+boolean deposit(float amt1,int accno)
+{
+for(int i=0;i<count;i++)
+{
+if(accno==det[i][0])
+{
+bal[i]=bal[i]+amt1;
+return true;
+}
+}
+return false;
+}
+boolean withdraw(int accno,float amt1)
+{
+for(int i=0;i<count;i++)
+{
+if(accno==det[i][0])
+{
+bal[i]=bal[i]-amt1;
+return true;
+}
+}
+return false;
+}
+float search(int accno)
+{
+for(int i=0;i<count;i++)
+{
+if(accno==det[i][0])
+{
+return bal[i];
+}
+}
+return -1;
+}
+boolean search(int accno,int a)
+{
+for(int i=0;i<count;i++)
+{
+if(accno==det[i][0])
+{
+System.out.println("Name: " +name[i]);
+System.out.println("Current Balance: " +bal[i]);
+return true;
+}
+}
+return false;
+}
+public static void main(String[] args) throws Exception
+{
+Bank obj=new Bank();
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.print("Enter the Operation to be Performed\n1.New Account\n2.Deposit Money\n3.Withdraw Money\n4.Search\n5.Exit\n");
+int choice;
+choice=Integer.parseInt(br.readLine());
+do
+{
+switch(choice) {
+
+			case 1:
+				System.out.println("Enter Name: ");
+				String str=br.readLine();
+				System.out.println("Enter Type: ");
+				int type=Integer.parseInt(br.readLine());
+				System.out.println("Enter Starting Deposit: ");
+				float amt=Float.parseFloat(br.readLine());
+				obj.intialize(type,amt,str);
+			break;
+			
+			case 2:
+				System.out.println("Enter Account No ");
+				int accno=Integer.parseInt(br.readLine());
+				System.out.println("Enter Amount");
+				float am1=Float.parseFloat(br.readLine());
+				boolean state=obj.deposit(am1,accno);
+				if(state==true)
+				System.out.println("Deposit Successful");
+				else
+				System.out.println("Invalid Details");
+			break;
+			
+			case 3:
+				System.out.println("Enter Account No ");
+				int accn=Integer.parseInt(br.readLine());
+				System.out.println("The Available Balance is: "+obj.search(accn));
+				System.out.println("Enter the amount for Withrawl");
+				float amt2=Float.parseFloat(br.readLine());
+				if(amt2<=obj.search(accn))
+				{
+				state=obj.withdraw(accn,amt2);
+				if(state==true)
+				System.out.println("WithDrawl Successful");
+				else
+				System.out.println("Invalid Details");
+				}
+				else
+				System.out.println("Invalid Amount");
+			break;
+			
+			case 4:
+				System.out.println("Enter Account No ");
+				accn=Integer.parseInt(br.readLine());
+				state=obj.search(accn,1);
+				if(state==false)
+				System.out.println("Invalid Details");
+			break;
+}
+
+
+System.out.print("Enter the Operation to be Performed\n1.New Account\n2.Deposit Money\n3.Withdraw Money\n4.Search\n5.Exit\n");
+choice=Integer.parseInt(br.readLine());
+}while(choice!=5);
+for(int i=0;i<obj.count;i++)
+{
+System.out.print("Acc No. "+obj.det[i][0]+"\n"+"Type: "+obj.det[i][1]+"\n"+"Balance Available: "+obj.bal[i]+"\n"+"Name: "+obj.name[i]+"\n");
+}
+}
+}
